4f203917bb289cb845c3153625b17952957d2a4d
AdithyaR9 <34142351+AdithyaR9@users.noreply.github.com>
2021-07-31 08:14:00 -0500

    Delete Life Org directory

--- Life Org/public/js/enter.js
+++
@@ -1,8 +0,0 @@
-function showPwdTxt() {
-    const x = document.getElementById("pwdShow");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
